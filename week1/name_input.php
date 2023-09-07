0c8c5d080a103680128840a40768a7f21fd13e63
Kyle Zhao <kylezhao101@gmail.com>
2023-09-07 12:15:53 -0700

    add form

--- week1/name_input.php
+++ week1/name_input.php
@@ -1,26 +1,43 @@
 <html>
-  <head>
-    <title>PHP Simple Form Processing</title>
-  </head>
 
-  <body>
-    <?php
-      if(isset($_POST['first_name']) && isset($_POST['last_name'])) {
+<head>
+  <title>PHP Simple Form Processing</title>
+</head>
 
-        if(empty($_POST['first_name']) || empty($_POST['last_name'])) {
-          echo '<p>You did not enter first name or last name</p>';
-        } else {
-          echo '<p>Your first name is <b>' . $_POST['first_name'] . '</b> and last name is <b>' . $_POST['last_name'] . '</b></p>';
-        }
-      }
-    ?>
-    <form method="post" action="name_input.php">
-      <lable>First Name:</lable>
-      <input size="20" name="first_name" type="text"/>
-      <lable>Last Name:</lable>
-      <input size="20" name="last_name" type="text"/>
-      <input type="submit" value="submit" />
-    </form>
-  </body>
+<body>
+  <?php
+  if (isset($_POST['school_name']) && isset($_POST['gender']) && isset($_POST['year'])) {
+
+    if (empty($_POST['school_name']) || empty($_POST['gender'])) {
+      echo '<p>Form incomplete</p>';
+    } else {
+      echo '<p>Your school is <b>' . $_POST['school_name'] . '</b> and gender is <b>' . $_POST['gender'] . '</b></p>';
+      echo '<p>Your year is <b> ' . $_POST['year'] . '</b></p>' ;
+    }
+  }
+  ?>
+  <form method="post" action="name_input.php">
+    <lable>School Name</lable>
+    <input size="20" name="school_name" type="text" />
+
+    <lable>Gender</lable>
+    <label for='male'>male</label>
+    <input type="radio" id="male" name="gender" value="male">
+    <label for='female'>female</label>
+    <input type="radio" id="female" name="gender" value="female">
+    <label for='other'>other</label>
+    <input type="radio" id="other" name="gender" value="other">
+
+    <label>School year</label>
+    <select name="year" id="year">
+      <option value="first">1</option>
+      <option value="second">2</option>
+      <option value="third">3</option>
+      <option value="fourth">4</option>
+      <option value="fifth and more">5+</option>
+    </select>
+    <input type="submit" value="submit" />
+  </form>
+</body>
 
 </html>
