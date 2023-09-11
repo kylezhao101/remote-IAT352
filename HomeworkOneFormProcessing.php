a36033750371173f7f52b48dbb7aae7830d6c33d
Kyle Zhao <kylezhao101@gmail.com>
2023-09-11 15:39:31 -0700

    complete form

--- HomeworkOneFormProcessing.php
+++ HomeworkOneFormProcessing.php
@@ -1,43 +1,70 @@
 <html>
 
 <head>
-  <title>IAT352 lab 1</title>
+    <title>IAT352 Homework 1</title>
 </head>
 
-<body>
-  <?php
-  if (isset($_POST['school_name']) && isset($_POST['gender']) && isset($_POST['year'])) {
-
-    if (empty($_POST['school_name']) || empty($_POST['gender'])) {
-      echo '<p>Form incomplete</p>';
-    } else {
-      echo '<p>Your school is <b>' . $_POST['school_name'] . '</b> and gender is <b>' . $_POST['gender'] . '</b></p>';
-      echo '<p>Your year is <b> ' . $_POST['year'] . '</b></p>' ;
+<style>
+    form{
+        display:flex;
+        gap: 5rem;
     }
-  }
-  ?>
-  <form method="post" action="name_input.php">
-    <lable>School Name</lable>
-    <input size="20" name="school_name" type="text" />
+    .formCol{
+        display:flex;
+        gap: 0.5rem;
+        flex-direction: column;
+    }
+</style>
 
-    <lable>Gender</lable>
-    <label for='male'>male</label>
-    <input type="radio" id="male" name="gender" value="male">
-    <label for='female'>female</label>
-    <input type="radio" id="female" name="gender" value="female">
-    <label for='other'>other</label>
-    <input type="radio" id="other" name="gender" value="other">
+<body>
+    <?php
+    if (isset($_POST['school_name']) && isset($_POST['gender']) && isset($_POST['year'])) {
 
-    <label>School year</label>
-    <select name="year" id="year">
-      <option value="first">1</option>
-      <option value="second">2</option>
-      <option value="third">3</option>
-      <option value="fourth">4</option>
-      <option value="fifth and more">5+</option>
-    </select>
-    <input type="submit" value="submit" />
-  </form>
+        if (empty($_POST['school_name']) || empty($_POST['gender'])) {
+            echo '<p>Form incomplete</p>';
+        } else {
+            echo '<p>Your school is <b>' . $_POST['school_name'] . '</b> and gender is <b>' . $_POST['gender'] . '</b></p>';
+            echo '<p>Your year is <b> ' . $_POST['year'] . '</b></p>';
+        }
+    }
+    ?>
+    <form method="post" action="HomeworkOneFormProcessing.php">
+        <form>
+            <div class='formCol'>
+                <label>Type of Program</label>
+                <select name='type' id='type'>
+                    <option value='exchange'>Exchange</option>
+                </select>
+
+                <label>Country</label>
+                <select name="country" id="country">
+
+                </select>
+
+                <label>Term</label>
+                <select name="term" id="term">
+                    <option value="Fall">Fall</option>
+                    <option value="Spring">Spring</option>
+                    <option value="Summer">Summer</option>
+                </select>
+            </div>
+            <div class='formCol'>
+                <label>Language of Instruction</label>
+                <select name="language" id="language">
+
+                </select>
+
+                <label>Level of Study (at SFU)</label>
+                <select name="level" id="level">
+                    <option value="Undergraduate">Undergraduate</option>
+                    <option value="Graduate">Graduate</option>
+                    <option value="PhdOrPostDoctoral">Phd or Post-Doctoral</option>
+                    <option value="PDP">PDP</option>
+                </select>
+            </div>
+        </form>
+        <input type="submit" value="apply" />
+    </form>
 </body>
 
 </html>
