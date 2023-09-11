4783b2482cdf92ce9ba41dc045f0ec14a4a5ad25
Kyle Zhao <kylezhao101@gmail.com>
2023-09-11 15:54:21 -0700

    add placeholder form option

--- HomeworkOneFormProcessing.php
+++ HomeworkOneFormProcessing.php
@@ -5,44 +5,33 @@
 </head>
 
 <style>
-    form{
-        display:flex;
-        gap: 5rem;
-    }
-    .formCol{
-        display:flex;
+    .formCol {
+        display: flex;
         gap: 0.5rem;
         flex-direction: column;
     }
 </style>
 
 <body>
-    <?php
-    if (isset($_POST['school_name']) && isset($_POST['gender']) && isset($_POST['year'])) {
-
-        if (empty($_POST['school_name']) || empty($_POST['gender'])) {
-            echo '<p>Form incomplete</p>';
-        } else {
-            echo '<p>Your school is <b>' . $_POST['school_name'] . '</b> and gender is <b>' . $_POST['gender'] . '</b></p>';
-            echo '<p>Your year is <b> ' . $_POST['year'] . '</b></p>';
-        }
-    }
-    ?>
     <form method="post" action="HomeworkOneFormProcessing.php">
-        <form>
+        <div class='formContainer'>
             <div class='formCol'>
                 <label>Type of Program</label>
                 <select name='type' id='type'>
+                    <option value='' disabled selected>Select your country of choice</option>
                     <option value='exchange'>Exchange</option>
                 </select>
 
                 <label>Country</label>
                 <select name="country" id="country">
+                    <option value='' disabled selected>Select your country of choice</option>
 
+                    <option value='Canada'>Canada</option>
                 </select>
 
                 <label>Term</label>
                 <select name="term" id="term">
+                    <option value='' disabled selected>Select your term</option>
                     <option value="Fall">Fall</option>
                     <option value="Spring">Spring</option>
                     <option value="Summer">Summer</option>
@@ -51,7 +40,9 @@
             <div class='formCol'>
                 <label>Language of Instruction</label>
                 <select name="language" id="language">
+                    <option value='' disabled selected>Select your language of choice</option>
 
+                    <option value='english'>English</option>
                 </select>
 
                 <label>Level of Study (at SFU)</label>
@@ -62,9 +53,19 @@
                     <option value="PDP">PDP</option>
                 </select>
             </div>
-        </form>
-        <input type="submit" value="apply" />
+            <div>
+                <input type="submit" value="apply" />
     </form>
+    <?php
+    if (isset($_POST['type']) && isset($_POST['term']) && isset($_POST['level'])) {
+
+        if (empty($_POST['type']) || empty($_POST['term']) || empty($_POST['level'])) {
+            echo '<p>Form incomplete</p>';
+        } else {
+            echo '<p>Your application is successfuly submitted. The following information is listed in the application</p>';
+        }
+    }
+    ?>
 </body>
 
 </html>
