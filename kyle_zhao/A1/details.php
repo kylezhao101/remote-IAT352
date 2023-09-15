9d309c9268cd59847d959d1f1e2d84e6239839ac
Kyle Zhao <kylezhao101@gmail.com>
2023-09-15 15:09:57 -0700

    create details.php

---
+++ kyle_zhao/A1/details.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html land='en'>
+    <head>
+</head>
+<body>
+    <?php
+        if (isset($_GET['id'])) {
+            $recipeId = $_GET['id'];
+
+            $csvFile = fopen();
+            // TODOS
+            // find line with id
+            // when found display information
+
+            // decode tags, description, ingredients, and instructions
+        }
+
+    ?>
+</body>
+</html
