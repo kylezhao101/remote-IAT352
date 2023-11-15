f0a56e351708d885e77e77ca60d385baca645d3b
PenguinBot <80377808+ThomasL08@users.noreply.github.com>
2023-11-14 21:45:21 -0800

    Update register.php
    made fixes its working perfectly now and adds user into the database as well

--- A4/register.php
+++ A4/register.php
@@ -1,22 +1,13 @@
 <?php
-// require_once('inialize.php');
 session_start();
 $dbserver = "localhost";
 $dbusername = "root";
 $dbpassword = "";
 $dbname = "classicmodels";
 
-$dbhost = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
+$db = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
 
-
-
-
-
-// END TODO
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // TODO: check for existing user account, if there is none, encrypt the password and save the entry
-    // Make sure password matches
-    // After the entry is inserted successfully, redirect to dashboard page
     if (
         isset($_POST['first_name']) &&
         isset($_POST['last_name']) &&
@@ -25,65 +16,43 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         isset($_POST['password']) &&
         isset($_POST['password_confirm'])
     ) {
-        if ($_POST['password'] == $_POST['password_confirm']) {
-            $sql = "select count(*) as count from admins where username=?";
+        // echo "Password: " . $_POST['password'] . "<br>";
+        // echo "Password Confirm: " . $_POST['password_confirm'] . "<br>";
+        if ($_POST['password'] === $_POST['password_confirm']) {
+            $sql = "SELECT COUNT(*) AS count FROM users WHERE email=?";
             $stmt = mysqli_prepare($db, $sql);
-            mysqli_stmt_bind_param($stmt, "s", $_POST["username"]);
+            mysqli_stmt_bind_param($stmt, "s", $_POST["email"]);
             mysqli_stmt_execute($stmt);
             $result = mysqli_stmt_get_result($stmt);
+
             if ($result) {
                 $res = mysqli_fetch_assoc($result);
+
                 if ($res['count'] > 0) {
-                    echo "This username is taken.";
+                    echo "This email is already registered.";
                 } else {
                     $hash_pass = password_hash($_POST["password"], PASSWORD_DEFAULT);
-                    $sql = "insert into admins (first_name, last_name, email, username, hashed_password) 
-            values (?,?,?,?,?)";
+                    $sql = "INSERT INTO users (first_name, last_name, email, encrypted_password) 
+                            VALUES (?, ?, ?, ?)";
                     $stmt = mysqli_prepare($db, $sql);
                     mysqli_stmt_bind_param(
                         $stmt,
-                        "sssss",
+                        "ssss",
                         $_POST["first_name"],
                         $_POST['last_name'],
                         $_POST['email'],
-                        $_POST['username'],
                         $hash_pass
                     );
                     $res = mysqli_stmt_execute($stmt);
                     if ($res) {
                         $_SESSION['username'] = $_POST['username'];
-                        header("Location: index.php");
+                        echo "Successfully registered!";
                     }
                 }
             }
+        } else {
+            echo "Passwords do not match.";
         }
-        // END TODO
     }
 }
-
 ?>
-
-<?php $page_title = 'Register'; ?>
-<?php include('header.php'); ?>
-
-<div id="content">
-    <h1>Register</h1>
-
-
-
-    <form action="register.php" method="post">
-        First Name:<br />
-        <input type="text" name="first_name" value="" required /><br />
-        Last Name:<br />
-        <input type="text" name="last_name" value="" required /><br />
-        Email:<br />
-        <input type="text" name="email" value="" required /><br />
-        Username:<br />
-        <input type="text" name="username" value="" required /><br />
-        Password:<br />
-        <input type="password" name="password" value="" required /><br />
-        Confirm Password:<br />
-        <input type="password" name="password_confirm" value="" required /><br />
-        <input type="submit" />
-    </form>
-</div>
