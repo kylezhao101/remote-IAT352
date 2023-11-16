1323cb2618229f295be140d0acfb279b1f48a8bb
kylezhao101 <kylezhao101@gmail.com>
2023-11-15 16:39:12 -0800

    display registration form

--- A4/register.php
+++ A4/register.php
@@ -52,6 +52,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     if ($res) {
                         $_SESSION['username'] = $_POST['username'];
                         echo "Successfully registered!";
+
+                        //redirect to login on success
+                        header("Location: login.php");
                     }
                 }
             }
@@ -61,3 +64,54 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport">
+    <title>Register</title>
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+    <h1>Register</h1>
+
+    <form action="register.php" method="post">
+        <label for="first_name">First Name:</label>
+        <br>
+        <input type="text" name="first_name" required>
+        <br>
+
+        <label for="last_name">Last Name:</label>
+        <br>
+        <input type="text" name="last_name" required>
+        <br>
+
+        <label for="email">Email:</label>
+        <br>
+        <input type="email" name="email" required>
+        <br>
+
+        <label for="username">Username:</label>
+        <br>
+        <input type="text" name="username" required>
+        <br>
+
+        <label for="password">Password:</label>
+        <br>
+        <input type="password" name="password" required>
+        <br>
+
+        <label for="password_confirm">Confirm Password:</label>
+        <br>
+        <input type="password" name="password_confirm" required>
+        <br>
+
+        <button type="submit">Register</button>
+    </form>
+
+</body>
+
+</html>
