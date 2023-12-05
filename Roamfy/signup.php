2dce14be8f8a56badeddc922c65d3ecb1932c7b3
PenguinBot <80377808+ThomasL08@users.noreply.github.com>
2023-12-04 20:45:56 -0800

    Add files via upload
    Made updates

---
+++ Roamfy/signup.php
@@ -0,0 +1,146 @@
+<?php
+session_start();
+
+function connectToDatabase() {
+    $dbserver = "localhost";
+    $dbusername = "root";
+    $dbpassword = "";
+    $dbname = "classicmodels";
+
+    $db = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
+
+    if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
+    }
+
+    return $db;
+}
+
+function redirectToHttps() {
+    if ($_SERVER["HTTPS"] != "on") {
+        header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+        exit();
+    }
+}
+
+function registerUser($db, $firstName, $lastName, $email, $username, $password, $passwordConfirm) {
+    // Check if passwords match
+    if ($password !== $passwordConfirm) {
+        return "Passwords do not match.";
+    }
+
+    // Check if email is already registered
+    $sql = "SELECT COUNT(*) AS count FROM users WHERE email=?";
+    $stmt = $db->prepare($sql);
+
+    if (!$stmt) {
+        return "Error preparing statement: " . $db->error;
+    }
+
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result) {
+        $count = mysqli_fetch_assoc($result)['count'];
+
+        if ($count > 0) {
+            return "This email is already registered.";
+        } else {
+            // Insert user into the database
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO users (first_name, last_name, email, username, encrypted_password) 
+                    VALUES (?, ?, ?, ?, ?)";
+            $stmt = $db->prepare($sql);
+
+            if (!$stmt) {
+                return "Error preparing statement: " . $db->error;
+            }
+
+            $stmt->bind_param("sssss", $firstName, $lastName, $email, $username, $hashedPassword);
+            $result = $stmt->execute();
+
+            if ($result) {
+                $_SESSION['username'] = $username;
+                header("Location: login.php");
+                exit();
+            } else {
+                return "Error registering user: " . $db->error;
+            }
+        }
+    }
+
+    return "Error checking email availability: " . $db->error;
+}
+
+function handleRegistrationForm() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $requiredFields = ['first_name', 'last_name', 'email', 'username', 'password', 'password_confirm'];
+
+        foreach ($requiredFields as $field) {
+            if (!isset($_POST[$field])) {
+                return "All fields are required.";
+            }
+        }
+
+        $db = connectToDatabase();
+        $result = registerUser(
+            $db,
+            $_POST['first_name'],
+            $_POST['last_name'],
+            $_POST['email'],
+            $_POST['username'],
+            $_POST['password'],
+            $_POST['password_confirm']
+        );
+
+        if ($result !== true) {
+            echo $result;
+        }
+    }
+}
+
+redirectToHttps();
+handleRegistrationForm();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport">
+    <title>Register</title>
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+    <h1>Register</h1>
+
+    <form action="register.php" method="post">
+        <!-- Add CSRF token for security -->
+        <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars(session_id()); ?>">
+
+        <label for="first_name">First Name:</label><br>
+        <input type="text" name="first_name" required><br>
+
+        <label for="last_name">Last Name:</label><br>
+        <input type="text" name="last_name" required><br>
+
+        <label for="email">Email:</label><br>
+        <input type="email" name="email" required><br>
+
+        <label for="username">Username:</label><br>
+        <input type="text" name="username" required><br>
+
+        <label for="password">Password:</label><br>
+        <input type="password" name="password" required><br>
+
+        <label for="password_confirm">Confirm Password:</label><br>
+        <input type="password" name="password_confirm" required><br>
+
+        <button type="submit">Register</button>
+    </form>
+</body>
+
+</html>
